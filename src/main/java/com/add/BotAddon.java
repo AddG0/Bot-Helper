ea3d97e62646b1338e4b4f6db00d528907e0ff5b
AddGamer09 <addgamer09@gmail.com>
2023-07-05 16:43:04 +0200

    Updated botaddon and gitignore

--- src/main/java/com/add/BotAddon.java
+++ src/main/java/com/add/BotAddon.java
@@ -4,5 +4,6 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public abstract class BotAddon extends ListenerAdapter {
-    public abstract void upsertCommands(JDA jda);
+    public void upsertCommands(JDA jda) {
+    }
 }
