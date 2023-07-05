a9e38ef3e9e77993bd302c69fa98da0aa735d858
AddGamer09 <addgamer09@gmail.com>
2023-07-05 17:55:33 +0200

    Updated Error Handler

--- src/main/java/com/add/ErrorHandler.java
+++ src/main/java/com/add/ErrorHandler.java
@@ -1,10 +1,14 @@
 package com.add;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 
 public class ErrorHandler {
+    private static Logger logger = LoggerFactory.getLogger(ErrorHandler.class.getName());
     private static ErrorHandlerInterface errorHandler = new ErrorHandlerInterface() {
     };
 
@@ -12,17 +16,19 @@ public class ErrorHandler {
     }
 
     public interface ErrorHandlerInterface {
-        public default void onError(Guild guild, String message) {
+        public default void onError(Guild guild, String message, Throwable e) {
             TextChannel errorChannel = guild
                     .getTextChannelById("" + ServerSQL.get(guild.getIdLong(), ServerOptions.BOT_ERROR_CHANNEL));
             if (errorChannel != null && errorChannel.canTalk()) {
                 errorChannel.sendMessage("" + message).queue();
             }
+            logger.error("Error: ", message, e.getStackTrace());
         }
 
-        public default void onError(SlashCommandInteractionEvent event, String message) {
+        public default void onError(SlashCommandInteractionEvent event, String message, Throwable e) {
             long errorId = Helper.generateId();
-            System.err.println("Error id: " + errorId);
+            logger.error("ErrorId: ", errorId);
+            logger.error("Error: ",  message, e.getStackTrace());
             event.getChannel()
                     .sendMessage("" + message + " Error id: `" + errorId + "`. Keep this if choose to contact us!")
                     .queue();
@@ -33,12 +39,12 @@ public class ErrorHandler {
         ErrorHandler.errorHandler = errorHandler;
     }
 
-    public static void sendErrorMessage(Guild guild, String message) {
-        errorHandler.onError(guild, message);
+    public static void sendErrorMessage(Guild guild, String message, Throwable e) {
+        errorHandler.onError(guild, message, e);
     }
 
-    public static void sendErrorMessage(SlashCommandInteractionEvent event, String message) {
-        errorHandler.onError(event, message);
+    public static void sendErrorMessage(SlashCommandInteractionEvent event, String message,Throwable e) {
+        errorHandler.onError(event, message, e);
     }
 
 }
