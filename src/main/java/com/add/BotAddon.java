3668d689b199ba49261f60d046ba3f586ebb7879
AddGamer09 <addgamer09@gmail.com>
2023-07-06 10:47:36 +0200

    Updated botaddon to use shardmanager

--- src/main/java/com/add/BotAddon.java
+++ src/main/java/com/add/BotAddon.java
@@ -2,8 +2,18 @@ package com.add;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.sharding.ShardManager;
 
 public abstract class BotAddon extends ListenerAdapter {
-    public void upsertCommands(JDA jda) {
+    public void upsertCommands(ShardManager shardManager) {
+    }
+
+    public void upsertGlobalCommands(ShardManager shardManager, CommandData commands) {
+        if (commands == null)
+            return;
+        for (JDA jda : shardManager.getShards()) {
+            jda.upsertCommand(commands).queue();
+        }
     }
 }
